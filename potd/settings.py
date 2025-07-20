c97112f67ee2843c8b1b818f56d1bb261e616891
Anas <86509653+AnasDharar@users.noreply.github.com>
2025-07-20 18:47:17 +0530

    dont ever use decouple

--- potd/settings.py
+++ potd/settings.py
@@ -9,7 +9,6 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-from decouple import config, Csv
 from pathlib import Path
 import dj_database_url
 import os
@@ -23,12 +22,12 @@ tmpPostgres = urlparse(os.getenv("DATABASE_URL"))
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+DEBUG = os.environ.get('DEBUG')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG',default=False, cast=bool)
 
-# ALLOWED_HOSTS = config('ALLOWED_HOSTS',cast=Csv())
+# ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', cast=Csv())
 ALLOWED_HOSTS = ['192.168.41.184','localhost','127.0.0.1']
 
 NPM_BIN_PATH = "C:\\Program Files\\nodejs\\npm.cmd"
@@ -80,7 +79,6 @@ WSGI_APPLICATION = 'potd.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-DEBUG = config('DEBUG', cast=bool)
 
 DATABASES = {
     'default': {
