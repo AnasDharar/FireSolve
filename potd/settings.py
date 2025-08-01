8bf06cf891d089843b08959d73013e4566cf6a85
Anas <86509653+AnasDharar@users.noreply.github.com>
2025-08-01 17:15:14 +0530

    logging dict

--- potd/settings.py
+++ potd/settings.py
@@ -51,7 +51,21 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
-
+# ------------------------------------------------------------------
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'level': 'DEBUG',
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'root': {
+        'handlers': ['console'],
+        'level': 'DEBUG',
+    },
+}
 # ------------------------------------------------------------------
 # Middleware
 # ------------------------------------------------------------------
